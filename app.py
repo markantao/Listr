eaa675ffaaebe43471b97bc1754d3d634a95335d
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-05-27 20:09:19 -0400

    created first part - Going to add DB

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def landing():
     return render_template('index.html')
 
 
+@app.route('/signup')
+def register():
+    return render_template('register.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
